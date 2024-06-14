caa5fac4eab7805a449a12fcb47210bb39088c85
shivamparihar12 <parihar.shivam0789@gmail.com>
2024-06-14 07:20:46 +0530

    client.py setuped

---
+++ client.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+# Client details
+HOST = '127.0.0.1'  # Localhost
+PORT = 12345        # Port to connect to
+
+# Create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+nickname = input("Choose your nickname: ")
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
